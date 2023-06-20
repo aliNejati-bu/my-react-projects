724d6633364b2548bb574817770894b7f5e0b1a2
aliNejati-bu <electrocellco@gmail.com>
2023-06-21 03:00:18 +0330

    001 full add login page.

--- 001/simple-login/src/Form/Login.js
+++ 001/simple-login/src/Form/Login.js
@@ -1,16 +1,24 @@
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 
+import classes from './Login.module.css';
+
 const Login = (props) => {
     return (
-        <Card>
-            <div>Login</div>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email"/>
-
-            <label htmlFor="password">Password</label>
-            <input type="text" id="password"/>
+        <Card className={classes.login}>
+            <h3 className='header'>Login</h3>
+            <div className={classes['form-control']}>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email"/>
+            </div>
+            <div className={classes['form-control']}>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password"/>
+            </div>
             <Button>Login</Button>
+            <p className={classes.register}>
+               do you haven't account? <a href='#'>Register</a>
+            </p>
         </Card>
     );
 }
--- 001/simple-login/src/UI/Card/Card.js
+++ 001/simple-login/src/UI/Card/Card.js
@@ -2,7 +2,7 @@ import classes from "./Card.module.css";
 
 const Card = props => {
     return (
-        <div className={classes.card}>
+        <div className={`${classes.card} ${props.className}`}>
             {props.children}
         </div>
     );
