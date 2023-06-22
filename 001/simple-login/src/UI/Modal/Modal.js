2529b89eaddcd849d84ec8f37c188ad77bbdf06f
aliNejati-bu <electrocellco@gmail.com>
2023-06-22 09:12:29 +0330

    init commit.

---
+++ 001/simple-login/src/UI/Modal/Modal.js
@@ -0,0 +1,12 @@
+import Card from "../Card/Card";
+
+const Modal = (props)=>{
+    return (
+        <>
+            <div/>
+            <Card>
+                
+            </Card>
+        </>
+    );
+}
