45b954f338a30576193704f8b83e8bf51a453041
aliNejati-bu <electrocellco@gmail.com>
2023-07-02 01:13:45 +0330

    manage entered data.

--- 001/simple-login/src/Form/Login.js
+++ 001/simple-login/src/Form/Login.js
@@ -2,22 +2,36 @@ import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 
 import classes from './Login.module.css';
+import {useState} from "react";
 
-const Login = (props) => {
+const Login = () => {
+
+    /* maneged entered email */
+    const [inputEmail, setInputEmail] = useState('');
+    const emailChangeHandle = (ev) => {
+        setInputEmail(ev.target.value);
+    }
+
+    /* manage password */
+    const [passwordInput, setPasswordInput] = useState('');
+    const passwordChangeHandler = (ev) => {
+        setPasswordInput(ev.target.value);
+    }
+
     return (
         <Card className={classes.login}>
             <h3 className='header'>Login</h3>
             <div className={classes['form-control']}>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email"/>
+                <input type="text" id="email" onChange={emailChangeHandle} value={inputEmail}/>
             </div>
             <div className={classes['form-control']}>
                 <label htmlFor="password">Password</label>
-                <input type="text" id="password"/>
+                <input type="text" id="password" onChange={passwordChangeHandler} value={passwordInput}/>
             </div>
             <Button>Login</Button>
             <p className={classes.register}>
-               do you haven't account? <a href='#'>Register</a>
+                do you haven't account? <a href="#">Register</a>
             </p>
         </Card>
     );
