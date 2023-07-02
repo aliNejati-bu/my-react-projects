b4354050de183a30efb9fd71d0904afb666510fb
aliNejati-bu <electrocellco@gmail.com>
2023-07-02 11:44:39 +0330

    001 manage input using refs.

--- 001/simple-login/src/Form/Login.js
+++ 001/simple-login/src/Form/Login.js
@@ -2,36 +2,28 @@ import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 
 import classes from './Login.module.css';
-import {useState} from "react";
+import {useState, useRef} from "react";
 import Modal from "../UI/Modal/Modal";
 
 const Login = () => {
+    const emailInput = useRef();
+    const passwordInput = useRef();
 
-    /* maneged entered email */
-    const [inputEmail, setInputEmail] = useState('');
-    const emailChangeHandle = (ev) => {
-        setInputEmail(ev.target.value);
-    }
-
-    /* manage password */
-    const [passwordInput, setPasswordInput] = useState('');
-    const passwordChangeHandler = (ev) => {
-        setPasswordInput(ev.target.value);
-    }
-
-    const [error,setError] = useState(false);
+    const [error, setError] = useState(false);
 
 
-    const submitHandler = (ev)=>{
+    const submitHandler = (ev) => {
         ev.preventDefault();
-        if (inputEmail.trim().length === 0 || passwordInput.trim().length === 0)
+        const inputEmail = emailInput.current.value;
+        const inputPassword = passwordInput.current.value;
+        if (inputEmail.trim().length === 0 || inputPassword.trim().length === 0)
             return setError({
-                title:'length',
+                title: 'length',
                 body: 'email or password can not empty.'
             });
         if (!inputEmail.includes('@'))
             setError({
-                title:'invalid Email.',
+                title: 'invalid Email.',
                 body: `Email ${inputEmail} is Invalid.`
             });
     };
@@ -39,7 +31,7 @@ const Login = () => {
     return (
         <form onSubmit={submitHandler}>
             {error &&
-                <Modal title={error.title} body={error.body} onConfirm={()=>{
+                <Modal title={error.title} body={error.body} onConfirm={() => {
                     setError(false)
                 }}/>
             }
@@ -47,11 +39,11 @@ const Login = () => {
                 <h3 className='header'>Login</h3>
                 <div className={classes['form-control']}>
                     <label htmlFor="email">Email</label>
-                    <input type="text" id="email" onChange={emailChangeHandle} value={inputEmail}/>
+                    <input type="text" id="email" ref={emailInput}/>
                 </div>
                 <div className={classes['form-control']}>
                     <label htmlFor="password">Password</label>
-                    <input type="text" id="password" onChange={passwordChangeHandler} value={passwordInput}/>
+                    <input type="text" id="password" ref={passwordInput}/>
                 </div>
                 <Button type='submit'>Login</Button>
                 <p className={classes.register}>
