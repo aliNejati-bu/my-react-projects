7b7fb70d9125d150225787b73d1396b326dde77f
aliNejati-bu <electrocellco@gmail.com>
2023-07-02 11:33:16 +0330

    001 modal using portal.

--- 001/simple-login/src/UI/Modal/Modal.js
+++ 001/simple-login/src/UI/Modal/Modal.js
@@ -1,3 +1,5 @@
+import ReactDOM from "react-dom";
+
 import Card from "../Card/Card";
 import classes from "./Modal.module.css";
 import Button from "../Button/Button";
@@ -5,18 +7,23 @@ import Button from "../Button/Button";
 const Modal = (props) => {
     return (
         <>
-            <div onClick={props.onConfirm} className={classes.back}/>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.body}>
-                    {props.body}
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {
+                ReactDOM.createPortal(<div onClick={props.onConfirm} className={classes.back}/>,document.getElementById('back-drop'))
+            }
+
+            {
+                ReactDOM.createPortal(<Card className={classes.modal}>
+                        <header className={classes.header}>
+                            <h2>{props.title}</h2>
+                        </header>
+                        <div className={classes.body}>
+                            {props.body}
+                        </div>
+                        <footer className={classes.actions}>
+                            <Button onClick={props.onConfirm}>Okay</Button>
+                        </footer>
+                    </Card>,document.getElementById('modal'))
+            }
         </>
     );
 }
